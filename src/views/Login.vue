7a9abf03acc80ccb4bf2ba05a087cc9f1c3b476a
Benjamin De Mot <benjamindm@hotmail.fr>
2021-05-11 15:48:19 +0200

    FRONT/Form "added Vuelidate and forms"

---
+++ src/views/Login.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="registerForm">
+    <h1>Connectez-vous : </h1>
+    <form @submit.prevent="submit">
+      <label for="email"> Votre email </label> <br>
+      <input type="email" id="email" name="email" v-model="user.email"> <br>
+
+      <label for="password"> Votre mot de passe </label> <br>
+      <input type="password" id="password" name="password" v-model="user.password"> <br>
+      <input type="submit" value="Submit">
+    </form>
+  </div>
+</template>
+
+<script>
+import { required, email, minLength } from 'vuelidate/lib/validators'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      user: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  validations: {
+    user: {
+      email: { required, email },
+      password: { required, minLength: minLength(7) }
+    }
+  },
+  methods: {
+    submit () {
+      if (this.$v.$invalid) {
+        console.log(this.$v.$invalid)
+      } else {
+        console.log(JSON.stringify(this.user))
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.registerForm{
+  background-color: white;
+  border: 1px solid black;
+  width: 50%;
+  min-width: 300px;
+  margin: auto;
+  margin-top: 10vh;
+  padding: 20px;
+  box-shadow: 5px 5px black;
+  & input{
+    margin-bottom: 20px;
+    width: 50%;
+    min-width: 300px;
+    height: 30px;
+    border: 1px solid black;
+    &:last-of-type{
+      box-shadow: 2px 2px black;
+      transition: all 100ms ease;
+      width: 20%;
+      max-width: 150px;
+      background-color: white;
+      border: 1px solid black;
+      &:hover{
+        box-shadow: 0 0 white;
+      }
+      &:active{
+        box-shadow: inset 2px 2px black;
+        background-image: radial-gradient(rgba(0, 0, 0, 0.4) .5px, transparent 0);
+        background-size: 2.5px 2.5px;
+      }
+    }
+  }
+}
+</style>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,5 +1,90 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="registerForm">
+    <h1>Inscrivez-vous : </h1>
+    <form @submit.prevent="submit">
+      <label for="pseudo"> Votre pseudonyme </label> <br>
+      <input type="text" id="pseudo" name="pseudo" v-model="user.pseudo"> <br>
+
+      <label for="email"> Votre email </label> <br>
+      <input type="email" id="email" name="email" v-model="user.email"> <br>
+
+      <label for="password"> Votre mot de passe </label> <br>
+      <input type="password" id="password" name="password" v-model="user.password"> <br>
+
+      <label for="confirmPassword"> Confirmez votre mot de passe </label> <br>
+      <input type="password" id="confirmPassword" name="confirmPassword" v-model="user.confirmPassword"> <br>
+      <input type="submit" value="Submit">
+    </form>
   </div>
 </template>
+
+<script>
+import { required, email, minLength, sameAs } from 'vuelidate/lib/validators'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      user: {
+        pseudo: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      }
+    }
+  },
+  validations: {
+    user: {
+      pseudo: { required },
+      email: { required, email },
+      password: { required, minLength: minLength(7) },
+      confirmPassword: { sameAsPassword: sameAs('password') }
+    }
+  },
+  methods: {
+    submit () {
+      if (this.$v.$invalid) {
+        console.log(this.$v.$invalid)
+      } else {
+        console.log(JSON.stringify(this.user))
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.registerForm{
+  background-color: white;
+  border: 1px solid black;
+  width: 50%;
+  min-width: 300px;
+  margin: auto;
+  margin-top: 10vh;
+  padding: 20px;
+  box-shadow: 5px 5px black;
+  & input{
+    margin-bottom: 20px;
+    width: 50%;
+    min-width: 300px;
+    height: 30px;
+    border: 1px solid black;
+    &:last-of-type{
+      box-shadow: 2px 2px black;
+      transition: all 100ms ease;
+      width: 20%;
+      max-width: 150px;
+      background-color: white;
+      border: 1px solid black;
+      &:hover{
+        box-shadow: 0 0 white;
+      }
+      &:active{
+        box-shadow: inset 2px 2px black;
+        background-image: radial-gradient(rgba(0, 0, 0, 0.4) .5px, transparent 0);
+        background-size: 2.5px 2.5px;
+      }
+    }
+  }
+}
+</style>
