b9c4a7ae10de4a42b6b2243cf5c5d35f2ab6a574
Benjamin De Mot <benjamindm@hotmail.fr>
2021-07-04 00:09:41 +0200

    BACK : fixed pb with password checking

--- BACK/controllers/users.js
+++ BACK/controllers/users.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt')
 const { v4: uuidv4 } = require('uuid');
 const jwt = require('jsonwebtoken')
 
-const regexAlphanum = !/[A-Za-z0-9]/g
+const regexAlphanum = /[A-Za-z0-9]/g
 const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
 exports.addUser = (req, res, next) => {
@@ -53,23 +53,33 @@ exports.loginUser = (req, res, next) => {
           res.status(404).json({ message : error.sqlMessage })
         }
         if (results) {
-          const correctPassword = bcrypt.compare(req.body.password, results[0].password)
-
-          if(results.length >= 1 && correctPassword) {
-            res.status(200).json({ 
-              message : 'Connexion établie !',
-              userPseudo: results[0].user,
-              privilege: results[0].privilege,
-              token: jwt.sign(
-                { userId: results[0].id },
-                process.env.TOKEN_SECRET,
-                { expiresIn: '12h' }
-              )})
-          } else {
-            console.log(hash)
+          if (typeof results[0] === "undefined") {
             res.status(401).json({ message : "L'email ou le mot de passe sont erronés." })
+            console.log("email doesn't exist on database")
+          } else {
+            bcrypt.compare(req.body.password, results[0].password)
+            .then(valid => {
+              if(!valid) {
+                res.status(401).json({ message : "L'email ou le mot de passe sont erronés." })
+                console.log("Password sent in request and the one stored in db doesn't match")
+              } else {
+                res.status(200).json({ 
+                  message : 'Connexion établie !',
+                  userPseudo: results[0].user,
+                  privilege: results[0].privilege,
+                  token: jwt.sign(
+                    { userId: results[0].id },
+                    process.env.TOKEN_SECRET,
+                    { expiresIn: '12h' }
+                  )
+                })
+                console.log("connection established")
+              }
+            })
+            .catch(error => res.status(500).json({ error }))
           }
         }
+        
         if(fields) {
           console.log("User querying a connexion")
         }
